66bf304feecf26312f96800c362ecdd9da4c9c22
peng.zhang <peng.zhang@travelzen.com>
2015-12-17 15:14:56 +0800

    add performance file log

--- src/main/java/com/zpeng/servlet/ServletDemo.java
+++ src/main/java/com/zpeng/servlet/ServletDemo.java
@@ -1,6 +1,7 @@
 package com.zpeng.servlet;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.zpeng.common.Constants;
 
 /**
  * 好久没写过servelt,jsp，重温下
@@ -19,14 +22,18 @@ import org.slf4j.LoggerFactory;
 public class ServletDemo extends HttpServlet{
 	
 	private static final Logger logger = LoggerFactory.getLogger(ServletDemo.class);
+	private static final Logger performance_logger = LoggerFactory.getLogger(Constants.PERFORMANCE_LOGGER_NAME);
 
 	private static final long serialVersionUID = 8637112013021873834L;
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		logger.debug("testServlet service.");
+		performance_logger.info("开始：{}", new Date());
+		long startTimesatmp = System.currentTimeMillis();
 		request.setAttribute("name", "figer");
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
+		performance_logger.info("结束：{}ms", System.currentTimeMillis() - startTimesatmp);
 	}
 
 	@Override
